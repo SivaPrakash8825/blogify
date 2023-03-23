9c57cd3bbf1f2e29685158ac3dc6d177d6ba6470
SivaPrakash8825 <sivaprakash2122003@gmail.com>
2023-03-23 17:52:23 +0530

    tom8

--- view/script.js
+++ view/script.js
@@ -1,4 +1,4 @@
-import axios from "./node_modules/axios";
+import axios from "../view/node_modules/axios";
 
 const text = document.querySelectorAll(".container h1");
 const email = document.querySelector("#reg-mail");
